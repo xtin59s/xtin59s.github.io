256bc361c31d4fcb97de0ec9215c1ed9273078fa
xtin59 <xtin59@gmail.com>
2022-09-19 20:59:15 +0800

    update blog

--- js/base.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const themeBtn = document.querySelectorAll('.theme-btn');
-
-for (let i = 0; i < themeBtn.length; i++) {
-    themeBtn[i].addEventListener('click', function () {
-        console.log('hello');
-    })
-}
